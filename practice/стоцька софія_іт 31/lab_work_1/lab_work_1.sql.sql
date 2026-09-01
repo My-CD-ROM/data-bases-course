84f5eeeb24c9d222460309370aaab34ee6627d9e
Sofia-Stotska <sofiastotska6@gmail.com>
2026-09-01 16:03:20 +0300

    Практична 1

---
+++ practice/стоцька софія_іт 31/lab_work_1/lab_work_1.sql.sql
@@ -0,0 +1,20 @@
+INSERT INTO medicines (name, manufacturer, form, price, stock_quantity)
+VALUES ('Paracetamol', 'Farmak', 'tablets', 45.50, 120);
+
+INSERT INTO medicines (name, manufacturer, form, price, stock_quantity)
+VALUES ('Ibuprofen', 'Darnytsia', 'tablets', 68.00, 85);
+
+INSERT INTO medicines (name, manufacturer, form, price, stock_quantity)
+VALUES ('Validol', 'Arterium', 'tablets', 32.75, 60);
+
+INSERT INTO medicines (name, manufacturer, form, price, stock_quantity)
+VALUES ('Ambroxol', 'Farmak', 'syrup', 95.20, 40);
+
+INSERT INTO medicines (name, manufacturer, form, price, stock_quantity)
+VALUES ('Chlorhexidine', 'Darnytsia', 'solution', 38.90, 75);
+
+INSERT INTO medicines (name, manufacturer, form, price, stock_quantity)
+VALUES ('Nurofen', 'Reckitt', 'capsules', 125.00, 35);
+ SELECT * FROM medicines;
+PRAGMA table_info(medicines);
+SELECT * FROM medicines;
